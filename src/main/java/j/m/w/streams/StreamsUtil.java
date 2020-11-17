77c6d4a5d93d8c86e36648005e9878cf8ba666fd
jasonwieczorek <wieczorek.jason@gmail.com>
2020-11-16 21:39:03 -0600

    just committing some new methods

--- src/main/java/j/m/w/streams/StreamsUtil.java
+++ src/main/java/j/m/w/streams/StreamsUtil.java
@@ -1,14 +1,16 @@
 package j.m.w.streams;
 
-import java.util.Comparator;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 /**
- * Just a simple util class that uses streams to do things
+ * Just a simple util class that uses streams to do things.
  */
 public class StreamsUtil {
-
 
     /**
      * Finds an element from a stream, returns null if not found
@@ -16,5 +18,30 @@ public class StreamsUtil {
     public static <T> T findElementFromStream(T searchObject, Stream<T> stream) {
 
         return (T) stream.filter(element -> element.equals(searchObject)).findFirst().orElse(null);
+    }
+
+    /**
+     * Simple Supplier that returns a LocalDate given an offset.
+     */
+    public static LocalDate supplyDateWithOffset(int offset) {
+
+        Supplier<LocalDate> dateSupplier = () -> {
+
+            // put complex logic here if supplier is a real complicated one
+            return LocalDate.now();
+        };
+
+        return dateSupplier.get().minusDays(offset);
+    }
+
+    /**
+     * Simple test of a Consumer, nom nom converts a list to a map with the keys as indexes
+     */
+    public static void consumeListToMap(List<String> list, Map<Integer, String> map) {
+
+        BiConsumer<Integer, String> mapConsumer = map::put;
+
+        final int[] index = { 0 };
+        list.forEach((element) -> mapConsumer.accept(index[0]++, element));
     }
 }
--- src/test/java/j/m/w/streams/StreamsUtilTest.java
+++ src/test/java/j/m/w/streams/StreamsUtilTest.java
@@ -3,6 +3,10 @@ package j.m.w.streams;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamsUtilTest {
@@ -12,16 +16,32 @@ public class StreamsUtilTest {
     @Test
     public void findElementFromStream_Found() {
 
-        String smallest =  StreamsUtil.findElementFromStream("Frog", animals);
+        String smallest = StreamsUtil.findElementFromStream("Frog", animals);
         Assert.assertEquals("Frog", smallest);
     }
 
     @Test
     public void findElementFromStream_NotFound() {
 
-        String smallest =  StreamsUtil.findElementFromStream("IDon'tExist", animals);
+        String smallest = StreamsUtil.findElementFromStream("IDon'tExist", animals);
         Assert.assertNull(smallest);
     }
 
+    @Test
+    public void supplyDateWithOffset() {
 
+        LocalDate today = LocalDate.now();
+        LocalDate suppliedDate = StreamsUtil.supplyDateWithOffset(1);
+
+        Assert.assertEquals(today.minusDays(1), suppliedDate);
+    }
+
+    @Test
+    public void consumeListToMap() {
+
+        Map<Integer, String> animalMap = new HashMap<>();
+        StreamsUtil.consumeListToMap(animals.collect(Collectors.toList()), animalMap);
+
+        Assert.assertEquals(4, animalMap.size());
+    }
 }
